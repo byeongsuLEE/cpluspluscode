cc20757784fb971b4583c4935efaab0e117f797a
byeongsuLEE <evil55@naver.com>
2021-01-07 05:34:53 +0900

    고양이 출력문제

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 
-	cout << "강한친구 대한육군" << endl;
-	cout << "강한친구 대한육군";
-	
+	cout << "\\    /\\ " <<endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|";
 }
