9770eeb4ae4629d4f80a6d3f842cbc2e980ae8e8
byeongsuLEE <evil55@naver.com>
2021-01-07 05:01:40 +0900

    수정완료

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 
 
-// https://github.com/byeongsuLEE/cpulscode.git
-int add(int a[], int size)
-{
-	int sum = 0;
-	for (int i = 0; i <= size; i++)
-	{
-		sum += i;
-	}
-	return sum;
-}
-
-
 int main()
 {
 
